7f47a47a252d330e04588f4199fe935ce194173d
unyime <unyimeusen01@gmail.com>
2023-01-26 17:08:03 +0100

    create routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Ticket from "./pages/Ticket";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/ticket" element={<Ticket />}></Route>
+          <Route
+            path="/ticket/:id"
+            element={<Ticket editMode={true} />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/AvatarDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AvatarDisplay = () => {
+  return <div>avatarDisplay</div>;
+};
+
+export default AvatarDisplay;
