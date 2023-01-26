5420ad7aef93e41fcd723a878453293c7e5b4061
unyime <unyimeusen01@gmail.com>
2023-01-26 17:39:01 +0100

    Create Navbar UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/svg/logo.svg";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const navigate = useNavigate();
+
+  return (
+    <nav>
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="controls-container">
+        <button className="icon" onClick={() => navigate("/ticket")}>
+          ＋
+        </button>
+        <button className="icon" onClick={() => navigate("/")}>
+          Back
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
